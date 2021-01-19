c0d5236e3710cfe393a36ab9721d145f0dbd8876
sakinah zahra <sakinahzahra4@gmail.com>
2021-01-19 21:26:24 +0700

    initial project version

--- IdealBMI.java
+++ IdealBMI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Example {
+public class IdealBMI {
    public static void main(String args[]) {
       Scanner sc = new Scanner(System.in);
       System.out.print("Input weight in kilogram: ");
